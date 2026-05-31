fc9e4c3ca2286f9d9b1d1a36cdce5ced8191b11b
jonolivertc <jon.olivertc@gmail.com>
2026-05-31 00:36:22 -0400

    Create page.tsx

---
+++ app/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24, fontFamily: "Arial" }}>
+      <h1>Father Figure Outreach Bot</h1>
+      <p>App is running.</p>
+    </main>
+  );
+}
